5cf7e34df1d58660681d310c08ac46358900eab3
Charlie Malmqvist <charlie.challe@live.se>
2020-04-28 17:38:03 +0200

    Premake optimization for release & dist

--- Wizzy/vendor/lua/premake5.lua
+++ Wizzy/vendor/lua/premake5.lua
@@ -24,3 +24,21 @@ project "lua"
   filter "system:macosx"
     systemversion "latest"
     staticruntime "On"
+
+  filter "configurations:Debug*"
+    defines "WZ_CONFIG_DEBUG"
+    runtime "Debug"
+    symbols "On"
+    optimize "Off"
+
+  filter "configurations:Release*"
+    defines "WZ_CONFIG_RELEASE"
+    runtime "Release"
+    symbols "On"
+    optimize "On"
+
+  filter "configurations:Dist*"
+    defines { "WZ_CONFIG_DIST", "WZ_DISABLE_ASSERTS" }
+    runtime "Release"
+    symbols "Off"
+    optimize "Speed"
--- premake5.lua
+++ premake5.lua
@@ -171,18 +171,19 @@ project "Wizzy"
     defines "WZ_CONFIG_DEBUG"
     runtime "Debug"
     symbols "On"
+    optimize "Off"
 
   filter "configurations:Release*"
     defines "WZ_CONFIG_RELEASE"
     runtime "Release"
-	symbols "On"
+	  symbols "On"
     optimize "On"
 
   filter "configurations:Dist*"
     defines { "WZ_CONFIG_DIST", "WZ_DISABLE_ASSERTS" }
     runtime "Release"
-	symbols "Off"
-    optimize "On"
+	  symbols "Off"
+    optimize "Speed"
 
 --[[------------------------------------------------------------------------------------]]
 
@@ -353,6 +354,7 @@ project "Sandbox"
     defines "WZ_CONFIG_DEBUG"
     runtime "Debug"
     symbols "On"
+    optimize "Off"
 
   filter "configurations:Release*"
     defines "WZ_CONFIG_RELEASE"
@@ -363,7 +365,7 @@ project "Sandbox"
   filter "configurations:Dist*"
     defines { "WZ_CONFIG_DIST", "WZ_DISABLE_ASSERTS" }
     runtime "Release"
-	symbols "Off"
-    optimize "On"
+    symbols "Off"
+    optimize "Speed"
 
 --[[------------------------------------------------------------------------------------]]
